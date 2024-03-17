d4b8b9bdbef45305b0d4e0d586183a3bfcdccae3
Syauqi <syauqi.d.u@gmail.com>
2024-03-17 16:29:17 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220112/Program.cs
+++ tpmodul5_1302220112/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace tpmodul5_1302220112;
+public class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " +  Data);
+    }
+
+}
--- tpmodul5_1302220112/obj/Debug/net8.0/tpmodul5_1302220112.AssemblyInfo.cs
+++ tpmodul5_1302220112/obj/Debug/net8.0/tpmodul5_1302220112.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302220112")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00ffdce4b0449ab375463b00b4654aed16524ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302220112")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302220112")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
